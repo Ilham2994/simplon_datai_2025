5ba5c3476ec203175c4b9fa1434d68f9ee5a662d
ilham24 <ilhambelbayda@gmail.com>
2023-09-14 09:12:04 +0200

    first commit

---
+++ 0/ilham.py
@@ -0,0 +1,12 @@
+def mettre_mots_en_majuscules(phrase):
+    
+    mots_en_majuscules = [mot.upper() for mot in mots]  
+    
+phrase_originale = "Ceci est un exemple de phrase en minuscules."
+resultat = mettre_mots_en_majuscules(phrase_originale)
+print(resultat) 
+
+    
+
+
+
